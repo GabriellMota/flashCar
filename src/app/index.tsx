1b3a6653d66302de6403c94a56825104807471d4
Gabriel Mota <vitor.216573@gmail.com>
2025-04-11 23:53:39 -0400

    screen sign in

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { Button } from '../components/button';
 
 export default function Index() {
     return (
-        <View className="flex-1 bg-black justify-center items-center">
+        <View className="flex-1">
             <Image
                 className="absolute w-full h-full"
                 source={require('../assets/background.png')}
                 resizeMode="cover"
             />
+            <View className='flex-1 justify-center p-6 gap-14'>
+                <View>
+                    <Text className='text-4xl text-COLORS-BRAND_MID font-bold'>Flash Car</Text>
+                    <Text className='text-xl text-white font-semibold opacity-50'>Venha dirigir com um aplicativo de cofiança</Text>
+                </View>
+                <Button title='Entrar com conta Google' />
+            </View>
         </View>
     );
 }
